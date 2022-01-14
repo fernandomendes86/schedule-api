0788579e87c392f8f4369bcee857df7f6f48072c
Fernando Mendes <fernando.msantos1@gmail.com>
2022-01-14 20:15:50 +0000

    Add rspec GET rooms

--- spec/controllers/rooms_spec.rb
+++ spec/controllers/rooms_spec.rb
@@ -1,11 +1,19 @@
 require "rails_helper"
 describe RoomsController, type: :controller do
   
-  describe "GET index" do
-    it "returns a 200" do
+  describe "Rooms HTTP Verb - Path" do
+
+    let(:room) { Room.find(1) }
+
+    it "GET INDEX and return :ok" do
       get :index
       expect(response).to have_http_status(:ok)
-      expect(response.status).to eql(200)
+    end
+
+    it "GET SHOW /rooms/:id" do
+      get :show, params: {id: room.id}
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:id]).to eql(room.id) 
     end
 
   end
