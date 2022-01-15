411f6133d8c85b1d2f04057c821ebafb91eb22b8
Fernando Mendes <fernando.msantos1@gmail.com>
2022-01-15 00:22:26 +0000

    add rspec create/update/delete to room

--- spec/controllers/rooms_spec.rb
+++ spec/controllers/rooms_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 describe RoomsController, type: :controller do
   
-  describe "Rooms HTTP Verb - Path" do
+  describe "Rooms - Actions" do
 
     let(:room) { Room.find(1) }
 
@@ -13,9 +13,36 @@ describe RoomsController, type: :controller do
     it "GET SHOW /rooms/:id" do
       get :show, params: {id: room.id}
       response_body = JSON.parse(response.body, symbolize_names: true)
-      expect(response_body[:id]).to eql(room.id) 
+      expect(response_body[:id]).to eq(room.id) 
     end
 
+    it "POST CREATE and return :created" do
+      start_time = Time.now
+      end_time = Time.now+8.hour
+      post :create, params: { room: { 
+        description: "Room Teste", start_time: start_time, end_time: end_time } }
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:created)
+    end
+
+    it "PATCH UPDATE and return :ok" do
+      patch :update, params: { id: room.id, room: { description: "Room Update"} }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "PUT UPDATE and return :ok" do
+      start_time = Time.now
+      end_time = Time.now+5.hour
+      put :update, params: { id: room.id, room: { 
+        description: "Room Update", start_time: start_time, end_time: end_time } }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "DELETE DESTROY and return No Content  " do
+      put :destroy, params: { id: room.id }
+      expect(response).to have_http_status(:no_content)
+    end
+    
   end
   
 end
